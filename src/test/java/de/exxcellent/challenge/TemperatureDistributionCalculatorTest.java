417d2549f59b037e59ff6570d263a93b70d2d42d
2Typpi <wrstle@web.de>
2021-10-15 18:21:31 +0200

    add Test for finding the day with the lowest temperature spread

---
+++ src/test/java/de/exxcellent/challenge/TemperatureDistributionCalculatorTest.java
@@ -0,0 +1,30 @@
+package de.exxcellent.challenge;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemperatureDistributionCalculatorTest {
+
+    @Test
+    void findMinTemperatureSpreadTest() {
+        WeatherDay dayOne = new WeatherDay(1, 88,59);
+        WeatherDay dayTwo = new WeatherDay(2, 79,63);
+        WeatherDay dayThree = new WeatherDay(3, 77,55);
+        WeatherDay lastDay = new WeatherDay(4, 77, 59);
+
+        List<WeatherDay> weatherData = new ArrayList();
+        weatherData.add(dayOne);
+        weatherData.add(dayTwo);
+        weatherData.add(dayThree);
+        weatherData.add(lastDay);
+
+        TemperatureDistributionCalculator temperatureDistributionCalculator = new TemperatureDistributionCalculator();
+        WeatherDay dayOfMinTemperatureSpread = temperatureDistributionCalculator.findMinTemperatureSpread(weatherData);
+
+        assertEquals(dayTwo.getDayNumber(), dayOfMinTemperatureSpread.getDayNumber(), "Day with Minimal Temperature Spread not found");
+    }
+}
