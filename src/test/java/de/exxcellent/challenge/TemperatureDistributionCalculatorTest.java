e3022199f9a30e3f4479a7e74da52a132727fbf3
2Typpi <wrstle@web.de>
2021-10-18 08:42:50 +0200

    add edge case tests for findMinTempSpread
    Test minTemperature negativ, maxTemperature and minTemperature negativ

--- src/test/java/de/exxcellent/challenge/TemperatureDistributionCalculatorTest.java
+++ src/test/java/de/exxcellent/challenge/TemperatureDistributionCalculatorTest.java
@@ -27,4 +27,42 @@ class TemperatureDistributionCalculatorTest {
 
         assertEquals(dayTwo.getDayNumber(), dayOfMinTemperatureSpread.getDayNumber(), "Day with Minimal Temperature Spread not found");
     }
+
+    @Test
+    void shouldFindMinTemperatureSpreadMinNegativeTest() {
+        WeatherDay dayOne = new WeatherDay(1, 5,-8);
+        WeatherDay dayTwo = new WeatherDay(2, 79,63);
+        WeatherDay dayThree = new WeatherDay(3, 77,55);
+        WeatherDay lastDay = new WeatherDay(4, 77, 59);
+
+        List<WeatherDay> weatherData = new ArrayList();
+        weatherData.add(dayOne);
+        weatherData.add(dayTwo);
+        weatherData.add(dayThree);
+        weatherData.add(lastDay);
+
+        TemperatureDistributionCalculator temperatureDistributionCalculator = new TemperatureDistributionCalculator();
+        WeatherDay dayOfMinTemperatureSpread = temperatureDistributionCalculator.findMinTemperatureSpread(weatherData);
+
+        assertEquals(dayOne.getDayNumber(), dayOfMinTemperatureSpread.getDayNumber(), "Day with Minimal Temperature Spread not found");
+    }
+
+    @Test
+    void shouldFindMinTemperatureSpreadDoubleNegativeTest() {
+        WeatherDay dayOne = new WeatherDay(1, 5,-8);
+        WeatherDay dayTwo = new WeatherDay(2, 79,63);
+        WeatherDay dayThree = new WeatherDay(3, -5,-9);
+        WeatherDay lastDay = new WeatherDay(4, 77, 59);
+
+        List<WeatherDay> weatherData = new ArrayList();
+        weatherData.add(dayOne);
+        weatherData.add(dayTwo);
+        weatherData.add(dayThree);
+        weatherData.add(lastDay);
+
+        TemperatureDistributionCalculator temperatureDistributionCalculator = new TemperatureDistributionCalculator();
+        WeatherDay dayOfMinTemperatureSpread = temperatureDistributionCalculator.findMinTemperatureSpread(weatherData);
+
+        assertEquals(dayThree.getDayNumber(), dayOfMinTemperatureSpread.getDayNumber(), "Day with Minimal Temperature Spread not found");
+    }
 }
